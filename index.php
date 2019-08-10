c8102672748449d584e286a649e2a62e136e3550
fhyuncai <fhyuncai@yuncaioo.com>
2019-08-10 19:34:28 +0800

    update to 1.0.2

--- index.php
+++ index.php
@@ -3,27 +3,27 @@
 /**
  * UptimeRobot-Page
  * A status page based on UptimeRobot
- * Version: 1.0.1
- * Update time: 2019-07-31
+ * Version: 1.0.2
+ * Update time: 2019-08-10
  * Author: FHYunCai(https://yuncaioo.com)
  **/
 
-$page_title = 'Service Status'; //Page title
-$cache_filename = 'uptimerobot.json'; //Cache filename
-$cache_timeout = 15; //Cache timeout(Minutes)
-$uptimerobot_apikey = ''; //Your UptimeRobot APIKey 
-$cron_key = 'fhyuncai'; //Cron key
+$page['title'] = 'Service Status'; //Page title
+$cache['filename'] = 'uptimerobot.json'; //Cache filename
+$cache['timeout'] = 15; //Cache timeout (Minutes)
+$setting['uptimerobot_apikey'] = ''; //Your UptimeRobot APIkey 
+$setting['cron_key'] = 'fhyuncai'; //Cron key
 
 date_default_timezone_set("PRC");
 
-$page_title = getenv('Page_Title')?getenv('Page_Title'):$page_title;
-$cache_filename = getenv('Cache_Filename')?getenv('Cache_Filename'):$cache_filename;
-$cache_timeout = getenv('Cache_Timeout')?getenv('Cache_Timeout'):$cache_timeout;
-$uptimerobot_apikey = getenv('UptimeRobot_APIKey')?getenv('UptimeRobot_APIKey'):$uptimerobot_apikey;
-$cron_key = getenv('Cron_Key')?getenv('Cron_Key'):$cron_key;
+$page['title'] = getenv('Page_Title')?getenv('Page_Title'):$page['title'];
+$cache['filename'] = getenv('Cache_Filename')?getenv('Cache_Filename'):$cache['filename'];
+$cache['timeout'] = getenv('Cache_Timeout')?getenv('Cache_Timeout'):$cache['timeout'];
+$setting['uptimerobot_apikey'] = getenv('UptimeRobot_APIKey')?getenv('UptimeRobot_APIKey'):$setting['uptimerobot_apikey'];
+$setting['cron_key'] = getenv('Cron_Key')?getenv('Cron_Key'):$setting['cron_key'];
 
 function curl_uptimerobot(){
-    global $uptimerobot_apikey;
+    global $setting;
     $curl = curl_init();
     curl_setopt_array($curl, array(
         CURLOPT_URL => 'https://api.uptimerobot.com/v2/getMonitors',
@@ -33,7 +33,7 @@ function curl_uptimerobot(){
         CURLOPT_TIMEOUT => 10,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => 'api_key='.$uptimerobot_apikey.'&format=json&logs=1',
+        CURLOPT_POSTFIELDS => 'api_key='.$setting['uptimerobot_apikey'].'&format=json&logs=1',
         CURLOPT_HTTPHEADER => array(
             'cache-control: no-cache',
             'content-type: application/x-www-form-urlencoded'
@@ -49,14 +49,14 @@ function curl_uptimerobot(){
     }
 }
 
-if(file_exists(__DIR__.'/'.$cache_filename)){
-    $json_last_time = json_decode(file_get_contents(__DIR__.'/'.$cache_filename))->time;
-    if($json_last_time < time()-($cache_timeout*60) || $_GET['cron'] == $cron_key){
+if(file_exists(__DIR__.'/'.$cache['filename'])){
+    $json_last_time = json_decode(file_get_contents(__DIR__.'/'.$cache['filename']))->time;
+    if($json_last_time < time()-($cache['timeout']*60) || $_GET['cron'] == $setting['cron_key']){
         $json_curl = curl_uptimerobot();
         if($json_curl == 'error'){
             die('Curl error!');
         }else{
-            $json_put = file_put_contents(__DIR__.'/'.$cache_filename,json_encode(array('time' => time(),'content' => json_decode($json_curl))));
+            $json_put = file_put_contents(__DIR__.'/'.$cache['filename'],json_encode(array('time' => time(),'content' => json_decode($json_curl))));
             $json_last_time = time();
         }
     }
@@ -65,17 +65,17 @@ if(file_exists(__DIR__.'/'.$cache_filename)){
     if($json_curl == 'error'){
         die('Curl error!');
     }else{
-        $json_put = file_put_contents(__DIR__.'/'.$cache_filename,json_encode(array('time' => time(),'content' => json_decode($json_curl))));
+        $json_put = file_put_contents(__DIR__.'/'.$cache['filename'],json_encode(array('time' => time(),'content' => json_decode($json_curl))));
         $json_last_time = time();
     }
 }
 
-if($_GET['cron'] != $cron_key){
-    echo '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width"><title>'.$page_title.'</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css"/><style>h1{font-weight:400;font-family:Helvetica;font-size:40px;margin-top:0px}.main-container{padding-top:30px;padding-bottom:80px}.icon-status{border-radius:100%}.icon{height:1em;width:1em;display:block;background-repeat:no-repeat;display:inline-block;margin: 7px 5px 0 0}</style></head><body class="mdui-color-blue-grey-50"><div class="mdui-container main-container"><div class="mdui-col-md-3"></div><div class="mdui-col-md-6"><h1 class="mdui-text-color-theme">'.$page_title.'</h1>';
+if($_GET['cron'] != $setting['cron_key']){
+    echo '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width"><title>'.$page['title'].'</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css"/><style>h1{font-weight:400;font-family:Helvetica;font-size:40px;margin-top:0px}.main-container{padding-top:30px;padding-bottom:80px}.icon-status{border-radius:100%}.icon{height:1em;width:1em;display:block;background-repeat:no-repeat;display:inline-block;margin: 7px 5px 0 0}</style></head><body class="mdui-color-blue-grey-50"><div class="mdui-container main-container"><div class="mdui-col-md-3"></div><div class="mdui-col-md-6"><h1 class="mdui-text-color-theme">'.$page['title'].'</h1>';
     echo '<p>Last check at '.date('Y-m-d H:i',$json_last_time).'</p>';
     echo '<div class="mdui-table-fluid">';
 
-    $json_decode = json_decode(file_get_contents(__DIR__.'/'.$cache_filename))->content;
+    $json_decode = json_decode(file_get_contents(__DIR__.'/'.$cache['filename']))->content;
 
     $group = array();
     foreach($json_decode->monitors as $monitor){
