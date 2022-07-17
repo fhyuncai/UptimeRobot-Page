0e82cb731896a6cfdb8fbfe80507131c92325f79
fhyuncai <fhyuncai@163.com>
2022-07-17 15:02:40 +0800

    optimize performance

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A status page based on [UptimeRobot](https://uptimerobot.com)
 
-![Version](https://img.shields.io/badge/Verison-1.31-blue?style=flat-square)
+![Version](https://img.shields.io/badge/Verison-1.4-blue?style=flat-square)
 ![GitHub](https://img.shields.io/github/license/fhyuncai/UptimeRobot-Page?style=flat-square)
 
 ## Requirements
@@ -18,8 +18,29 @@ curl -O https://raw.githubusercontent.com/fhyuncai/UptimeRobot-Page/master/index
 
 ## Configure
 
-Edit `index.php` configuration data directory, and access this file to configuration.
+See `Configuration` in `index.php`.
 
 ## Group Usage
 
 You can use Parser to group, And default Parser is `Group/Name`.
+
+# 中文说明
+
+## 环境要求
+
+* PHP version >= 5.4
+* UptimeRobot 账户
+
+## 部署
+
+```bash
+curl -O https://raw.githubusercontent.com/fhyuncai/UptimeRobot-Page/master/index.php
+```
+
+## 配置
+
+在 `index.php` 文件中的 `Configuration` 部分进行配置。
+
+## 监控组
+
+此状态页支持分组监控，默认格式为 `组/名称` ，在 UptimeRobot 控制台内设置形如此格式的 Friendly Name。
--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 /**
  * UptimeRobot-Page
  * Status page based on UptimeRobot
- * Version: 1.31
+ * Version: 1.4
  * Update time: 2022-07-17
  * Author: FHYunCai (https://yuncaioo.com)
  **/
 
 date_default_timezone_set('PRC');
 
-/* Config Start */
+/* Configuration Start */
 
 define('DATADIR', 'uptimerobot'); // Data directory
 
 // Note: The remaining configuration items are in the ([Data directory]/config.json)
 
-/* Config End */
+/* Configuration End */
 
 $record_time_s = explode(' ', microtime());
 
@@ -44,8 +44,8 @@ $config_arr = [
 if (empty($config_arr['uptimerobot_apikey'])) die('You must configure the Uptimerobot key in file <i>' . DATADIR . '/config.json</i>');
 
 //Read and update data
-if (is_file(__DIR__ . '/' . DATADIR . '/cache/cache.json') && $_GET['cron'] != $config_arr['cron_key']) {
-    $json_last_time = json_decode(file_get_contents(__DIR__ . '/' . DATADIR . '/cache/cache.json'))->time;
+if (is_file(__DIR__ . '/' . DATADIR . '/cache/data.json') && $_GET['cron'] != $config_arr['cron_key']) {
+    $json_last_time = json_decode(file_get_contents(__DIR__ . '/' . DATADIR . '/cache/data.json'))->time;
     if ($json_last_time < time() - ($config_arr['cache_timeout'] * 60)) {
         update_data();
         $json_last_time = time();
@@ -57,44 +57,17 @@ if (is_file(__DIR__ . '/' . DATADIR . '/cache/cache.json') && $_GET['cron'] != $
 
 
 if ($_GET['cron'] != $config_arr['cron_key']) {
-    //Calculate availability
-    for ($i = 1; $i <= 30; $i++) {
-        $date = date("Y/m/d", strtotime('-' . $i . ' day'));
-        if (is_dir(__DIR__ . '/' . DATADIR . '/cache/' . $date)) {
-            $cache_file_arr = scandir(__DIR__ . '/' . DATADIR . '/cache/' . $date);
-            foreach ($cache_file_arr as $value) {
-                if ($value == '.' || $value == '..') continue;
-                if (!strstr($value, '.json')) continue;
-                $json_str = file_get_contents(__DIR__ . '/' . DATADIR . '/cache/' . $date . '/' . $value);
-                check_json($json_str);
-                foreach (json_decode($json_str)->monitors as $value) {
-                    $cache_data_arr[$value->name][] = $value->status;
-                }
-            }
-            if (isset($cache_data_arr)) {
-                foreach ($cache_data_arr as $key => $value) {
-                    $check_num = count($value);
-                    $check_up = 0;
-                    foreach ($value as $value) {
-                        if ($value == 2) $check_up++;
-                    }
-                    $act_per_day_arr[$key][$i] = round($check_up / $check_num * 100, 2); //Percentage per day
-                }
-                unset($cache_data_arr);
-            }
-        }
-    }
-    if (isset($act_per_day_arr)) {
-        foreach ($act_per_day_arr as $key => $value) {
-            $act_per_arr[$key] = round(array_sum($value) / count($value), 2); //Percentage of 30days
-        }
-    }
+    if (!is_file(__DIR__ . '/' . DATADIR . '/cache/cache.json')) calculate();
+    $dataArr = json_decode(file_get_contents(__DIR__ . '/' . DATADIR . '/cache/cache.json'), true);
+    if (isset($dataArr['act_per_day_arr'])) $act_per_day_arr = $dataArr['act_per_day_arr'];
+    if (isset($dataArr['act_per_arr'])) $act_per_arr = $dataArr['act_per_arr'];
+
 
     echo '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport"content="width=device-width, initial-scale=1"><title>' . $config_arr['page_title'] . '</title><link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css"><script src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script><script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script><style>.theme{background:#eef2f6}.main{max-width:600px}.title{margin:1em 0;margin-left:1px}.desc{font-size:80%;margin-left:1px}.item-desc{font-size:.8em;margin-top:.8em;margin-bottom:.5em}.icon-status{height:1em;width:1em;display:block;margin-top:auto!important;margin-bottom:auto!important;background-repeat:no-repeat;border-radius:100%}.icon-uptime{height:1.5em;width:100%;margin:0 1px;opacity:.75}.icon-status.up,.icon-uptime.up{background:#6ac259}.icon-status.seemdown,.icon-uptime.seemdown{background:#ffdd57}.icon-status.down,.icon-uptime.down{background:#f05228}.icon-status.pause,.icon-uptime.pause{background:#111}.icon-uptime.nodata{background:#e5e5e5}</style></head>';
     echo '<body class="theme"><div class="container main mb-4"><h4 class="font-weight-normal title">' . $config_arr['page_title'] . '</h4><p class="desc">Last check at ' . date('Y-m-d H:i', $json_last_time) . '<p>';
     echo '<ul class="list-group">';
 
-    $json_decode = json_decode(file_get_contents(__DIR__ . '/' . DATADIR . '/cache/cache.json'));
+    $json_decode = json_decode(file_get_contents(__DIR__ . '/' . DATADIR . '/cache/data.json'));
 
     $group = [];
     foreach ($json_decode->monitors as $monitor) {
@@ -142,7 +115,8 @@ if ($_GET['cron'] != $config_arr['cron_key']) {
     $record_time_e = explode(' ', microtime());
     echo '<!--Exec time: ' . round($record_time_e[0] + $record_time_e[1] - ($record_time_s[0] + $record_time_s[1]), 5) . '-->';
 } else {
-    echo 'Data update success';
+    $record_time_e = explode(' ', microtime());
+    echo 'Data update success, Time: ' . round($record_time_e[0] + $record_time_e[1] - ($record_time_s[0] + $record_time_s[1]), 5);
 }
 
 
@@ -181,16 +155,56 @@ function curl_uptimerobot()
 function update_data()
 {
     $json_curl = curl_uptimerobot();
-    if (is_file(__DIR__ . '/' . DATADIR . '/cache/cache.json')) {
-        $json_old_time = json_decode(file_get_contents(__DIR__ . '/' . DATADIR . '/cache/cache.json'))->time;
-        copy(__DIR__ . '/' . DATADIR . '/cache/cache.json', __DIR__ . '/' . DATADIR . '/cache/' . date("Y/m/d", $json_old_time) . '/' . $json_old_time . '.json');
+    if (is_file(__DIR__ . '/' . DATADIR . '/cache/data.json')) {
+        $json_old_time = json_decode(file_get_contents(__DIR__ . '/' . DATADIR . '/cache/data.json'))->time;
+        copy(__DIR__ . '/' . DATADIR . '/cache/data.json', __DIR__ . '/' . DATADIR . '/cache/' . date("Y/m/d", $json_old_time) . '/' . $json_old_time . '.json');
     }
     $json_arr = json_decode($json_curl);
     foreach ($json_arr->monitors as $value) {
         $json_arr_monitors[] = ['name' => $value->friendly_name, 'url' => $value->url, 'status' => $value->status];
     }
-    file_put_contents(__DIR__ . '/' . DATADIR . '/cache/cache.json', json_encode(array('time' => time(), 'monitors' => $json_arr_monitors)));
+    file_put_contents(__DIR__ . '/' . DATADIR . '/cache/data.json', json_encode(array('time' => time(), 'monitors' => $json_arr_monitors)));
+    calculate();
     return;
+}
+
+function calculate()
+{
+    //Calculate availability
+    for ($i = 1; $i <= 30; $i++) {
+        $date = date("Y/m/d", strtotime('-' . $i . ' day'));
+        if (is_dir(__DIR__ . '/' . DATADIR . '/cache/' . $date)) {
+            $cache_file_arr = scandir(__DIR__ . '/' . DATADIR . '/cache/' . $date);
+            foreach ($cache_file_arr as $value) {
+                if ($value == '.' || $value == '..') continue;
+                if (!strstr($value, '.json')) continue;
+                $json_str = file_get_contents(__DIR__ . '/' . DATADIR . '/cache/' . $date . '/' . $value);
+                check_json($json_str);
+                foreach (json_decode($json_str)->monitors as $value) {
+                    $cache_data_arr[$value->name][] = $value->status;
+                }
+            }
+            if (isset($cache_data_arr)) {
+                foreach ($cache_data_arr as $key => $value) {
+                    $check_num = count($value);
+                    $check_up = 0;
+                    foreach ($value as $value) {
+                        if ($value == 2) $check_up++;
+                    }
+                    $act_per_day_arr[$key][$i] = round($check_up / $check_num * 100, 2); //Percentage per day
+                }
+                $saveData = ['act_per_day_arr' => $act_per_day_arr];
+                unset($cache_data_arr);
+            }
+        }
+    }
+    if (isset($act_per_day_arr)) {
+        foreach ($act_per_day_arr as $key => $value) {
+            $act_per_arr[$key] = round(array_sum($value) / count($value), 2); //Percentage of 30days
+        }
+        $saveData['act_per_arr'] = $act_per_arr;
+    }
+    file_put_contents(__DIR__ . '/' . DATADIR . '/cache/cache.json', json_encode($saveData));
 }
 
 function check_json($str)
